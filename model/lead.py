6317ab29ecadeaeb44292b5832d50a445c66a2ef
venkataraman chidambaram <infoappu@gmail.com>
2023-03-19 16:54:59 +0530

    Lead feature in progress
    * Update lead and get lead functionality done.

--- model/lead.py
+++ model/lead.py
@@ -1,6 +1,11 @@
-# Lead model implementation.
-
-
 # Insert lead query.
-def insert_lead():
-  print 'Lead inserted into database.
+def insert_lead_query():
+  print 'Lead inserted into database.'
+
+# Update lead query.
+def update_lead_query():
+  print 'Lead updated into database.'
+
+# Get lead query.
+def select_lead_query():
+  print 'Lead select from database.'
